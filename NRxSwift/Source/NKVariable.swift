ed61279dc4722b7eec969956d07c66f5e6960304
Nghia Phu Nguyen <nghianp@fastsell.vn>
2016-12-26 22:45:13 +0700

    Bump to 1.0.15

--- NRxSwift/Source/NKVariable.swift
+++ NRxSwift/Source/NKVariable.swift
@@ -10,17 +10,38 @@ import Foundation
 import RxSwift
 
 public struct NKVariable<T> {
-    private let variable: Variable<T>
+    private let observableClosure: () -> Observable<T>
+    private let variableClosure: () -> T
+    
+    public init(observableClosure: @escaping () -> Observable<T>, variableClosure: @escaping () -> T) {
+        self.observableClosure = observableClosure
+        self.variableClosure = variableClosure
+    }
     
     public init(variable: Variable<T>) {
-        self.variable = variable
+        self.observableClosure = {
+            return variable.asObservable()
+        }
+        
+        self.variableClosure = {
+            return variable.value
+        }
     }
     
     public var value: T {
-        return self.variable.value
+        return self.variableClosure()
     }
     
     public var observable: Observable<T> {
-        return self.variable.asObservable()
+        return self.observableClosure()
+    }
+    
+    public func map<S>(_ closure: @escaping (T) -> S) -> NKVariable<S> {
+        
+        return NKVariable<S>.init(observableClosure: {
+            return self.observableClosure().map(closure)
+        }, variableClosure: {
+            return closure(self.variableClosure())
+        })
     }
 }
